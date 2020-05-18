3b5b97c71dc834d28e9947b4e25d6fb8eb764def
kouhei <kobuncruisin185@yahoo.co.jp>
2020-05-18 22:11:56 +0900

    add-fix2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,7 +19,7 @@ $(function(){
             <img src=${message.image} >
           </div>`
       }
-       else {
+      else {
         var html =
          `<div class="message">
             <div class="upper-message">
@@ -36,8 +36,8 @@ $(function(){
               </p>
             </div>
           </div>`
-        return html;
       };
+    return html;
   };
 
   $('#new_message').on('submit', function(e){
